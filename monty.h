a5b96c0714805e6ac4f1aa0922b3a36570ba0fbc
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-20 12:30:36 +0000

    The header file that contains all the protoypes

---
+++ monty.h
@@ -0,0 +1,52 @@
+#ifndef MONTY_H
+#define MONTY_H
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern char *op_code[];
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+void monty_interpreter(unsigned int line_number);
+void tokenize_line(char *buf);
+void read_file(FILE *file_p);
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+
+#endif /* MONTY_H */
