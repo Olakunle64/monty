9616d7aa2d6cfcccd2562f04b9a5f686f9a6dc9c
Olakunle64 <olakunleisiaq50@gmail.com>
2023-09-21 13:27:45 +0000

    include the prototype of the enqueue function in the header file

--- monty.h
+++ monty.h
@@ -58,4 +58,6 @@ void _mod(stack_t **stack, unsigned int line_number);
 void _pchar(stack_t **stack, unsigned int line_number);
 void _pstr(stack_t **stack, unsigned int line_number);
 void _rotl(stack_t **stack, unsigned int line_number);
+void _enqueue(stack_t **stack, unsigned int line_number);
+
 #endif /* MONTY_H */
